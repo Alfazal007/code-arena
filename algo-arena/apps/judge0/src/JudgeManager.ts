3d5589d37ff79231ddccbe7d0d5dcd2ad08e5957
Alfazal007 <alfazal7777@gmail.com>
2024-12-08 02:03:42 +0530

    supplying stdin to judge0

--- algo-arena/apps/judge0/src/JudgeManager.ts
+++ algo-arena/apps/judge0/src/JudgeManager.ts
@@ -13,13 +13,19 @@ export class JudgeManager {
         return JudgeManager.instance;
     }
 
-    async createRustSummission({ sourceCode, stdin, testNo, programId }: { sourceCode: string, stdin: string, testNo: number, programId: string }) {
-        const res = await axios.post("https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=true&wait=true&fields=*",
+    async createRustSummission({ stdin, submissionInfo }: { submissionInfo: ReceivedSubmissionMessage, stdin: string[] }) {
+        const outputsRequired = this.readOutputFile(submissionInfo.problemName);
+        const inputsToJudge: { source_code: string, language_id: number, expected_output: string }[] = [];
+        for (let i = 0; i < outputsRequired.length; i++) {
+            inputsToJudge.push({
+                source_code: submissionInfo.submittedCode,
+                language_id: 73,
+                expected_output: outputsRequired[i] as string
+            });
+        }
+        const res = await axios.post("https://judge0-ce.p.rapidapi.com/submissions/batch?base64_encoded=true",
             {
-                "source_code": sourceCode,
-                "language_id": 73,//93
-                "stdin": stdin,
-                "cpu_time_limit": 2
+                "submissions": inputsToJudge
             },
             {
                 headers: {
@@ -28,14 +34,15 @@ export class JudgeManager {
                 }
             }
         );
-        console.log({ res: res.data })
-        // update in the database
+        if (res.status != 201) {
+            return;
+        }
     }
 
-    async createJSSummission({ sourceCode, stdin, testNo, programId }: { sourceCode: string, stdin: string, testNo: number, programId: string }) {
+    async createJSSummission({ stdin, submissionInfo }: { submissionInfo: ReceivedSubmissionMessage, stdin: string[] }) {
         const res = await axios.post("https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=true&wait=true&fields=*",
             {
-                "source_code": sourceCode,
+                "source_code": submissionInfo.submittedCode,
                 "language_id": 93,
                 "stdin": stdin,
                 "cpu_time_limit": 2
@@ -71,7 +78,33 @@ export class JudgeManager {
             console.error('Error reading test case files:', error);
             throw error;
         }
-        console.log(inputs)
+        if (submission.language == "rust") {
+            await this.createRustSummission({ submissionInfo: submission, stdin: inputs })
+        } else {
+            await this.createJSSummission({ submissionInfo: submission, stdin: inputs })
+        }
+    }
+
+    private readOutputFile(problemName: string) {
+        const outputFolder = path.resolve(__dirname, `../../problems/${problemName}/output`);
+        let outputs: string[] = [];
+        try {
+            if (!fs.existsSync(outputFolder)) {
+                throw new Error(`Directory does not exist: ${outputFolder}`);
+            }
+
+            const files = fs.readdirSync(outputFolder);
+            outputs = files
+                .filter(file => path.extname(file) === '.txt')
+                .map(file => {
+                    const filePath = path.join(outputFolder, file);
+                    return btoa(fs.readFileSync(filePath, 'utf8'));
+                });
+        } catch (error) {
+            console.error('Error reading test case files:', error);
+            throw error;
+        }
+        return outputs;
     }
 }
 
